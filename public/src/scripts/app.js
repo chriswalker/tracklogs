1b57f2d07e075dc6b69413042ccdeab420d42d7e
Chris Walker <thatamazingweb@gmail.com>
2017-01-22 21:27:50 +0000

    Remove receiveMessage() as belonged to Strava project code was copied from

--- public/src/scripts/app.js
+++ public/src/scripts/app.js
@@ -1,10 +1,13 @@
 'use strict'
 
-var Vue = require('vue')
+const Vue = require('vue')
+
 require('./components/map.js')
 require('./components/navigation.js')
 require('./components/sidebar.js')
+require('./components/logindialog.js')
 
+// TODO refactoring some of this
 var vm = new Vue({
   el: '#app',
   data: function () {
@@ -24,16 +27,3 @@ var vm = new Vue({
   }
 })
 
-// Sigh - postMessage() from the popup window sends messages to
-// window listeners, not DOM ones, so cannot respond to the message
-// from within the navbar component. Catch the window event and
-// generate a Vue one for the navbar component, to tell it to close
-// the window
-function receiveMessage (event) {
-  var origin = event.origin || event.originalEvent.origin
-  if (origin !== 'http://localhost:8000') {
-    return
-  }
-  vm.$broadcast('authenticated', event)
-}
-window.addEventListener('message', receiveMessage, false)
